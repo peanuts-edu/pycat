8aa7a634e6c74a1254e03d420ffe11e0528a0435
dwhite0 <white.dh@gmail.com>
2023-04-06 17:19:21 +0800

    bugfix: don't use level world position to set layer sprite positions

--- pycat/extensions/ldtk.py
+++ pycat/extensions/ldtk.py
@@ -69,6 +69,7 @@ class LdtkLayeredLevel:
             if level_data.id == level_id:
                 return cls(level_data, image_path, layer_ordering)
         print_warning('No level named '+level_id+' in file '+ldtk_file_path)
+        print_warning('Possible levels: '+', '.join([l.id for l in all_level_data]))
 
     def __init__(self, level_data: LevelData, image_path: str, layer_ordering: Dict[str,int]):
         self.image_path = image_path
@@ -85,10 +86,11 @@ class LdtkLayeredLevel:
                 layer=layer_order,
                 image=self.image_path+self.level_data.id+'__'+layer_name+'.png',
                 scale=scale)
-            sprite.x = self.level_data.x + sprite.width/2
-            sprite.y = self.level_data.y + sprite.height/2
+            sprite.x = sprite.width/2
+            sprite.y = sprite.height/2
             self.layer_sprites[layer_name] = sprite
 
+        tags_for_debug = set()
         for e in self.level_data.entities:
             s = window.create_sprite(
                 x=scale*e.x,
@@ -98,9 +100,18 @@ class LdtkLayeredLevel:
                 tags=['ldtk_'+t for t in e.tags],
                 opacity=100 if debug_entities else 0,
                 layer=debug_entities_layer)
+            
+            tags_for_debug.update(s.tags)
             if debug_entities:
                 s.color = Color.random_rgb()
                 label = window.create_label(text=str(s.tags), position=s.position, layer=debug_entities_layer)
                 label.position += Point(-label.content_width/2, label.content_height/2)
 
-            self.entities.append(s)  
+            self.entities.append(s)
+
+        if debug_entities:
+            print_warning('Rendering LDTK level')
+            print_warning('Level name: '+self.level_data.id)
+            print_warning('Tags in level: '+', '.join([str(tag) for tag in tags_for_debug]))
+
+        
